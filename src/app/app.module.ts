429223bd45fbf1dbdf9b0253205b2140bf20c045
larry <caneslarry@gmail.com>
2021-02-28 19:07:39 -0500

    add books component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { BooksComponent } from './books/books.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    BooksComponent
   ],
   imports: [
-    BrowserModule
+    DragDropModule,
+    FormsModule,
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'books', component: BooksComponent},
+      {path: 'angular-demo', component: BooksComponent},
+    ]),
+    MatCardModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+   bootstrap: [AppComponent, BooksComponent]
 })
 export class AppModule { }
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
-
+  book: string;
+  author: string;
+  publishdate: string;
+  books = [];
   constructor() { }
-
-  ngOnInit(): void {
+  onDrop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.books,
+      event.previousIndex,
+      event.currentIndex);
   }
-
+  addBook(): void {
+      this.books.push({title: this.book, author: this.author, publishdate: this.publishdate});
+      this.book = '';
+      this.author = '';
+      this.publishdate = '';
+  }
+  capitalizeTitles(): void {
+    const elements = document.querySelectorAll('.book-item .card-body');
+    // console.log(elements);
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      element.innerHTML = element.innerHTML.toUpperCase();
+    }
+  }
+  ngOnInit(): void {
+    this.books = [
+      {title: 'Title1', author: 'Author1', publishdate: '1/1/2020'},
+      {title: 'Title2', author: 'Author2', publishdate: '1/2/2020'},
+      {title: 'Title3', author: 'Author3', publishdate: '1/3/2020'},
+      {title: 'Title4', author: 'Author4', publishdate: '1/4/2020'},
+      {title: 'Title5', author: 'Author5', publishdate: '1/5/2020'},
+    ];
+  }
 }
